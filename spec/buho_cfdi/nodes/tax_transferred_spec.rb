14ea7dcb31609090f517cdcff8298a0f98c7dd5c
Juan Mex <juanmex.me@gmail.com>
2022-07-19 17:03:34 -0500

    Better expectations

--- spec/buho_cfdi/nodes/tax_transferred_spec.rb
+++ spec/buho_cfdi/nodes/tax_transferred_spec.rb
@@ -2,7 +2,7 @@ RSpec.describe ::Nodes::TaxTransferred, type: :model do
   context 'when factor_type is not Exento' do
     let (:node) { ::Nodes::TaxTransferred.new({tax: '001', factor_type: 'Tasa', base: '100', rate_or_fee: '0.1000', import: '100'}) }
     it 'has all values' do
-      expect(node.valid?).to eq(true)
+      expect(node).to be_valid
       expect(node.tax).to eq('001')
       expect(node.factor_type).to eq('Tasa')
       expect(node.base).to eq('100')
@@ -15,12 +15,12 @@ RSpec.describe ::Nodes::TaxTransferred, type: :model do
   context 'when factor_type is Exento' do
     let (:node) { ::Nodes::TaxTransferred.new({tax: '001', factor_type: 'Exento', base: '100'}) }
     it 'has only required values' do
-      expect(node.valid?).to be(true)
+      expect(node).to be_valid
       expect(node.tax).to eq('001')
       expect(node.factor_type).to eq('Exento')
       expect(node.base).to eq('100')
-      expect(node.rate_or_fee).to eq(nil)
-      expect(node.import).to eq(nil)
+      expect(node.rate_or_fee).to be_nil
+      expect(node.import).to be_nil
       expect(node.to_hash).to eq({"Impuesto"=>"001", "TipoFactor"=>"Exento", "Base"=>"100"})
     end
   end
